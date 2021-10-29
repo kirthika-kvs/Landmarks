adbb0b5c6603de51e1c6b776f5fe723424a6f441
kirthika-kvs <kirthika.kvs@gmail.com>
2021-10-29 12:24:00 +0530

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Saravanan Pari on 10/29/21.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
