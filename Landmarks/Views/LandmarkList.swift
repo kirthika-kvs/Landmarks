795a1381e944d56eb6c7c65e412231aa7f804fda
kirthika-kvs <kirthika.kvs@gmail.com>
2021-10-29 22:54:44 +0530

    Navigation Link & detail page view
    Update the detail page and link navigation list row to its destination. Pass data to destination detail view.

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -11,7 +11,7 @@ struct LandmarkList: View {
     var body: some View {
         NavigationView{
             List(landmarks) { landmark in
-                NavigationLink(destination: LandmarkDetail()){
+                NavigationLink(destination: LandmarkDetail(landmark: landmark)){
                     LandmarkRow(landmark: landmark)
                 }
             }
@@ -22,6 +22,11 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        ForEach(["iPhone SE (2nd generation)", "iPhone 12 Pro Max"], id: \.self) {
+            deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+        }
     }
 }
